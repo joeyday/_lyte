df2aaefc4b174473d4f19c17c21d8b2a5bcf1f9b
Joey Day <joeyday@gmail.com>
2015-03-17 14:59:54 -0600

    Stuff
    Changed some stuff.

--- header.php
+++ header.php
@@ -9,25 +9,33 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
-<title><?php wp_title( '‹', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<script type="text/javascript" src="//use.typekit.net/lkn6tyl.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<script src="/jquery-1.7.2.min.js" type="text/javascript"></script>
-<script src="/hyphenate-3.3.0.min.js" type="text/javascript"></script>
-<script type="text/javascript">
-			var hyphenatorSettings = {
-				selectorfunction: function () {
-					return $('div#content p').get();
-				}
-			};
-			Hyphenator.config(hyphenatorSettings);
-			Hyphenator.run();
-</script>
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php wp_title( '‹', true, 'right' ); ?></title>
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <meta name="msapplication-TileColor" content="#ee7120">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <script type="text/javascript" src="//use.typekit.net/lkn6tyl.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <script src="/bibly-hack.js"></script>
+    <link href="http://code.bib.ly/bibly.min.css" rel="stylesheet" />
+    <script src="/jquery-1.7.2.min.js" type="text/javascript"></script>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -37,7 +45,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<img src="/images/joeyday.png" alt="Joey Day" class="avatar" />
-			<p class="blurb">Hi, I’m Joey. I’m a 30-something <abbr title="Information Technology">IT</abbr> professional (read: big nerd) in Salt Lake City. When I grow up I’d like to be a seminary professor (read: even bigger nerd).</p>
+			<p class="blurb">Hi, I’m Joey. I’m a 30-something software engineer (read: big nerd) in Salt Lake City. When I grow up I’d like to be a seminary professor (read: even bigger nerd).</p>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
